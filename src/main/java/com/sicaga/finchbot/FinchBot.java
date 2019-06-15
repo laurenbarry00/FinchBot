53029fdbb0e7959474353df86e12aed5102aade6
Lauren Barry <laurenbarry00@gmail.com>
2019-06-14 18:08:30 -0700

    Config now loads from JSON into its own class, instead of main

--- src/main/java/com/sicaga/finchbot/FinchBot.java
+++ src/main/java/com/sicaga/finchbot/FinchBot.java
@@ -2,35 +2,39 @@ package com.sicaga.finchbot;
 
 import com.jagrosh.jdautilities.command.CommandClientBuilder;
 import com.jagrosh.jdautilities.commons.waiter.EventWaiter;
+import com.sicaga.finchbot.util.Config;
 import net.dv8tion.jda.core.AccountType;
+import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.Game;
 
 import javax.security.auth.login.LoginException;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
 public class FinchBot {
-    public static void main(String[] args) throws IOException, LoginException, IllegalArgumentException {
-        // config.txt has two lines: bot token and owner ID
-        List<String> configList = Files.readAllLines(Paths.get("src/config.txt"));
-        final String token = configList.get(0);
-        final String ownerId = configList.get(1);
+    public static JDA jda;
+    public static Config config;
+
+    public static void main(String[] args) throws LoginException, IllegalArgumentException {
+        // config holds token, owner id, and dev options
+        config = new Config();
+        config.load();
 
         // setting up an eventwaiter and the command client
         EventWaiter waiter = new EventWaiter();
         CommandClientBuilder client = new CommandClientBuilder();
 
         // default playing message is "Type <prefix>help"
-        client.useDefaultGame();
+        if (config.isDevModeEnabled()) {
+            client.setGame(Game.playing("Running in Dev Mode"));
+        } else {
+            client.useDefaultGame();
+        }
 
-        client.setOwnerId(ownerId);
+        client.setOwnerId(config.getOwnerId());
 
         // Set emojis for successes, warnings, and failures
-        client.setEmojis("\uD83D\uDE03", "\uD83D\uDE2E", "\uD83D\uDE26");
+        client.setEmojis("\u2705", "\u26A0", "\u274C");
 
         client.setPrefix("?");
 
@@ -44,14 +48,20 @@ public class FinchBot {
         new JDABuilder(AccountType.BOT)
                 .setToken(token)
 
+        jda = new JDABuilder(AccountType.BOT)
+                .setToken(config.getToken())
+
                 // set temporary status while bot is loading
                 .setStatus(OnlineStatus.DO_NOT_DISTURB)
                 .setGame(Game.playing("Loading..."))
 
                 // add the command and waiter listeners
+                // add the command and waiter event
                 .addEventListener(waiter)
                 .addEventListener(client.build())
 
+                // start it up!
                 .build();
+
     }
 }
---
+++ src/main/java/com/sicaga/finchbot/util/Config.java
@@ -0,0 +1,143 @@
+package com.sicaga.finchbot.util;
+
+import com.google.gson.*;
+import com.sicaga.finchbot.FinchBot;
+import net.dv8tion.jda.core.entities.Role;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Set;
+
+public class Config {
+    private String token;
+    private String ownerId;
+    private boolean devModeEnabled;
+    private ArrayList<String> devUserIds;
+    private HashMap<String, ArrayList<RoleEmotePair>> trackedMessages;
+
+    public Config() {
+        this.token = null;
+        this.ownerId = null;
+        this.devModeEnabled = false;
+        this.devUserIds = null;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    public String getOwnerId() {
+        return ownerId;
+    }
+
+    public void setOwnerId(String ownerId) {
+        this.ownerId = ownerId;
+    }
+
+    public boolean isDevModeEnabled() {
+        return devModeEnabled;
+    }
+
+    public void setDevModeEnabled(boolean devModeEnabled) {
+        this.devModeEnabled = devModeEnabled;
+    }
+
+    public ArrayList<String> getDevUserIds() {
+        return devUserIds;
+    }
+
+    public void addDevUserId(String devUserId) {
+        devUserIds.add(devUserId);
+    }
+
+    public void removeDevUserId(String devUserId) {
+        devUserIds.remove(devUserId);
+    }
+
+    public HashMap<String, ArrayList<RoleEmotePair>> getTrackedMessages() {
+        return trackedMessages;
+    }
+
+    public void load() {
+        try {
+            // For reading in the raw JSON from file
+            JsonParser parser = new JsonParser();
+
+            // Parse the json from file and create a JsonObject of the whole thing
+            JsonObject root = (JsonObject) parser.parse(new FileReader("src/config.json"));
+
+            // Get token, owner ID, and dev mode status
+            this.token = root.get("token").getAsString();
+            this.ownerId = root.get("ownerId").getAsString();
+            this.devModeEnabled = root.getAsJsonObject("dev").get("devModeEnabled").getAsBoolean();
+
+            //initialize the list
+            devUserIds = new ArrayList<>();
+            // convert list of dev user ids to a json array
+            JsonArray jsonDevIds = root.getAsJsonObject("dev").get("devUserIds").getAsJsonArray();
+            // Loop through json array and add the ids to our outward-facing ArrayList of dev user ids.
+            for (int i = 0; i < jsonDevIds.size(); i++) {
+                devUserIds.add(jsonDevIds.get(i).getAsString());
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void loadRoleEmotePairs() {
+        try {
+            // For reading in the raw JSON from file
+            JsonParser parser = new JsonParser();
+
+            // Parse the json from file and create a JsonObject of the whole thing
+            JsonObject root = (JsonObject) parser.parse(new FileReader("src/config.json"));
+
+            // initialize and populate the list of tracked message IDs
+            trackedMessages = new HashMap<>();
+            JsonObject jsonTrackedMessages = root.get("trackedMessages").getAsJsonObject();
+
+            // iterate through all the tracked messages
+            Set<String> messageKeys = jsonTrackedMessages.keySet();
+            for (String key : messageKeys) {
+                // JsonObject for the individual message that we're tracking
+                JsonObject message = jsonTrackedMessages.get(key).getAsJsonObject();
+
+                // this will be the list of RoleEmotePairs that are associated with the message
+                ArrayList<RoleEmotePair> reps = new ArrayList<>();
+
+                // all the roles associated with the message
+                Set<String> roles = jsonTrackedMessages.get(key).getAsJsonObject().keySet();
+
+                for (String roleKey : roles) {
+                    //
+                    JsonArray ar = message.get(roleKey).getAsJsonArray();
+
+                    // Get a role object from the role ID in the json
+                    Role role = FinchBot.jda.getGuildById(464369469595058176L).getRoleById(roleKey);
+
+                    // this is either the unicode or emote id
+                    String emote = ar.get(0).getAsString();
+
+                    // whether or not the bot should auto-remove the reaction after it's added by a user
+                    // (ie. for color roles)
+                    boolean shouldRemoveAfterAdding = ar.get(1).getAsBoolean();
+
+                    // create the RoleEmotePair and add it to the list
+                    RoleEmotePair rep = new RoleEmotePair(emote, role, shouldRemoveAfterAdding);
+                    reps.add(rep);
+                }
+                // Add the message and REP list to our hashmap
+                trackedMessages.put(key, reps);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
