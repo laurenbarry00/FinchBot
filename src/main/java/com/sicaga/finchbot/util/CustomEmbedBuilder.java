befd5b8969ceef6969fd07d45c6f94eb802b4d8a
laurenbarry00 <laurenbarry00@gmail.com>
2019-06-21 15:48:17 -0700

    Added timestamp to custom embedbuilder

--- src/main/java/com/sicaga/finchbot/util/CustomEmbedBuilder.java
+++ src/main/java/com/sicaga/finchbot/util/CustomEmbedBuilder.java
@@ -3,11 +3,13 @@ package com.sicaga.finchbot.util;
 import net.dv8tion.jda.core.EmbedBuilder;
 
 import java.awt.*;
+import java.time.Instant;
 
 public class CustomEmbedBuilder extends EmbedBuilder {
     public CustomEmbedBuilder() {
         this.setColor(new Color(255, 243, 17)); // TODO: Put Sicaga yellow color
         this.setFooter("Sicaga FinchBot", "https://cdn.discordapp.com/embed/avatars/0.png"); // TODO: Put image link for FinchBot icon
         this.setAuthor("FinchBot", "http://sicaga.com/#/", "https://cdn.discordapp.com/embed/avatars/0.png");
+        this.setTimestamp(Instant.now());
     }
 }
