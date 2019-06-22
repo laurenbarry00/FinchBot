fe29d65533a0f9f8d1d691aab90b2ab4acf17989
Lauren Barry <laurenbarry00@gmail.com>
2019-06-22 16:07:52 -0700

    Added setting presence online when message received

--- src/main/java/com/sicaga/finchbot/event/MessageReceivedListener.java
+++ src/main/java/com/sicaga/finchbot/event/MessageReceivedListener.java
@@ -1,6 +1,7 @@
 package com.sicaga.finchbot.event;
 
 import com.sicaga.finchbot.FinchBot;
+import net.dv8tion.jda.core.OnlineStatus;
 import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
@@ -8,6 +9,8 @@ import net.dv8tion.jda.core.hooks.ListenerAdapter;
 public class MessageReceivedListener extends ListenerAdapter {
     @Override
     public void onMessageReceived(MessageReceivedEvent event) {
+        FinchBot.jda.getPresence().setPresence(OnlineStatus.ONLINE, false); // In case the websocket get disconnected, otherwise the bot shows as offline
+
         if (FinchBot.config.isDevModeEnabled()) { // In dev mode, we only respond to users that are listed in the config
             User user = event.getAuthor();
             if (!FinchBot.config.getDevUserIds().contains(user.getId())) {
